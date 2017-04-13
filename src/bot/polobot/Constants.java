7920139c17c4c434bf87c5cbef3d0371f570fb86
PolosElite23 <jasondash23@gmail.com>
2017-04-13 15:30:59 -0400

    Update Constants.java

--- src/bot/polobot/Constants.java
+++ src/bot/polobot/Constants.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * Created by Jason on 4/10/2017.
  */
 public class Constants {
-    public static final String discordToken = "MjcxNDkyMzEyMzU2NDg3MTY4.C82tSw.gP1OjTQLjcGlFmJJnHWj3_2v4B4";
+    public static final String discordToken = "TOKEN HERE";
 
     public static ArrayList<String> remindList = new ArrayList<>();
 }
